46b0db3fd9364b3f22ef3edd555f2ac4d31e5023
michaelmirhom <micharby73@gmail.com>
2023-08-02 01:17:01 -0500

    completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,35 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word=None):
+        self.word = word
+
+    def match(self, word_list):
+        # Initialize an empty list to store matching words
+        matching_words = []
+
+        # Iterate through each word in the word_list
+        for word in word_list:
+            # Check if the word is an anagram of the initialized word
+            if self.is_anagram(self.word, word):
+                # If it is an anagram, add it to the matching_words list
+                matching_words.append(word)
+
+        return matching_words
+
+    def is_anagram(self, word1, word2):
+        # Convert both words to lowercase for case-insensitive comparison
+        word1 = word1.lower()
+        word2 = word2.lower()
+
+        # Check if the two words have the same characters when sorted
+        return sorted(word1) == sorted(word2)
+
+        
+
+     
+
+
+
+      
+ 
+ 
